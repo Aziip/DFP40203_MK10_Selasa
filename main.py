e3249e22e12226fc683ad8b00a924793a373548e
Azx2 <126233598+Aziip@users.noreply.github.com>
2023-11-07 12:00:11 +0800

    siap

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ files=os.listdir(folderpath)
 
 for file in files:
     print (file)
+
+old="C:/Users/DELL/PycharmProjects/DFP40203_MK10_Selasa/file/test.txt"
+new="C:/Users/DELL/PycharmProjects/DFP40203_MK10_Selasa/file/testing1.txt"
+
+os.rename(old,new)
+print(f"{old} has been renamed to{new}")
